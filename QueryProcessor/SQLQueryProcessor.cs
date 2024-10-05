5e3582be548690e31097fd975cba2990de35e7d6
luisfeliTEC-0603 <167169513+luisfeliTEC-0603@users.noreply.github.com>
2024-10-04 23:41:10 -0600

    Arguments Definition

---
+++ QueryProcessor/SQLQueryProcessor.cs
@@ -0,0 +1,96 @@
+﻿    using Entities;
+    using QueryProcessor.Exceptions;
+    using QueryProcessor.Operations;
+    using StoreDataManager;
+
+    namespace QueryProcessor
+    {
+        // CREAT DATABSE <dataBase-name>
+        // SET <dataBase-name>
+        // CREAT TABLE <table-name> ( arg, )
+        // DROP TABLE <table-name>
+        // SELECT ( *|<columns> ) FROM <table-name> [WHERE ( arg ) ORDER BY ( ASC/DEC )]
+        // UPDATE <table-name> SET <column-name> = <new-value> [WHERE ( arg )];
+        // DELETE FROM <table-name> [WHERE ( arg )]
+        // INSERT INTO <table-name> VALUES ( arg1, agr2, ... )
+        // INDEX ???
+
+        // Method to process SQL sentences
+        public class SQLQueryProcessor
+        {
+
+            public static OperationStatus Execute(string sentence)
+            {
+                // CREATE DATABASE SQL sentence
+                if (sentence.StartsWith("CREATE DATABASE"))
+                {
+                    directoryName = sentence.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)[2];
+
+                    return new CreateDataBase().Execute(directoryName);
+                }
+
+                // SET DATABASE SQL sentence
+                if (sentence.StartsWith("SET"))
+                {
+                    directoryName = sentence.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)[2];
+
+                    return new Set().Execute(directoryName);
+                }
+
+                // DROP DATABASE SQL sentence
+                if (sentence.StartsWith("DROP TABLE"))
+                {
+                    directoryName = sentence.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)[2];
+
+                    return new DropTable().Execute(directoryName);
+                }
+
+                // CREATE DATABASE SQL sentence
+                if (sentence.StartsWith("CREATE TABLE"))
+                {
+                    directoryName = sentence.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)[2];
+                    sentence = sentence.Replace($"CREATE TABLE {directoryName}", "").Trim().TrimEnd(';');
+
+                    if (sentence.StartsWith("(") && sentence.EndsWith(")"))
+                    {
+                        sentence = sentence.Substring(1, sentence.Length - 2).Trim();
+                    } 
+
+                    return new CreateTable().Execute(directoryName, sentence);
+                }
+
+                // UPDATE SQL sentence
+                if (sentence.StartsWith("UPDATE"))
+                {
+                    directoryName = sentence.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)[1];
+                    sentence = sentence.Replace($"CREATE TABLE {directoryName} SET", "").Trim().TrimEnd(';');
+
+                    selectedColumns = sentence.Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+
+                    whereIndex = sentence.IndexOf("WHERE");
+
+                    if (whereIndex != -1)
+                    {
+                        string columnAssignments = sentence.Substring(0, index).Trim();
+
+                        string whereClause = sentence.Substring(index + "WHERE".Length).Trim();
+                    }
+
+                    return new Update().Execute();
+                }
+
+                // SELECT DATABASE SQL sentence
+                if (sentence.StartsWith("SELECT"))
+                {
+                    //...
+
+                    return new Select().Execute();
+                }
+
+                else
+                {
+                    throw new UnknownSQLSentenceException();
+                }
+            }
+        }
+    }
---
+++ StoreDataManager/Store.cs
@@ -0,0 +1,123 @@
+﻿using Entities;
+using System.IO.Compression;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+
+namespace StoreDataManager
+{
+    public sealed class Store
+    {
+        private static Store? instance = null;
+        private static readonly object _lock = new object();
+               
+        public static Store GetInstance()
+        {
+            lock(_lock)
+            {
+                if (instance == null) 
+                {
+                    instance = new Store();
+                }
+                return instance;
+            }
+        }
+
+        private readonly string baseDirectory;
+        private readonly string DatabaseBasePath;
+        private readonly string DataPath;
+        private readonly string SystemCatalogPath;
+        private readonly string SystemDatabasesFile;
+        private readonly string SystemTablesFile;        
+        
+        public Store()
+        {
+            baseDirectory = Directory.GetCurrentDirectory();
+            DatabaseBasePath = Path.Combine(baseDirectory, "TinySQL");
+            SystemCatalogPath = Path.Combine(DatabaseBasePath, "SystemCatalog");
+            SystemDatabasesFile = Path.Combine(SystemCatalogPath, "SystemDatabases.table");
+            SystemTablesFile = Path.Combine(SystemCatalogPath, "SystemTables.table");
+
+            this.InitializeSystemCatalog();
+        }
+
+        private void InitializeSystemCatalog()
+        {
+            // Always make sure that the system catalog and above folder
+            // exist when initializing
+            Directory.CreateDirectory(SystemCatalogPath);
+        }
+
+        public OperationStatus CreateDataBase(string dataBase)
+        {
+            // Creates a new directory in the specified path 
+            Directory.CreateDirectory($@"{DataPath}\{dataBase}");
+            
+            return OperationStatus.Success;
+        }
+
+        public OperationStatus CreateTable()
+        {
+            // Creates a default DB called TESTDB
+            Directory.CreateDirectory($@"{DataPath}\TESTDB");
+
+            // Creates a default Table called ESTUDIANTES
+            var tablePath = $@"{DataPath}\TESTDB\ESTUDIANTES.Table";
+
+            using (FileStream stream = File.Open(tablePath, FileMode.OpenOrCreate))
+            using (BinaryWriter writer = new (stream))
+            {
+                // Create an object with a hardcoded.
+                // First field is an int, second field is a string of size 30,
+                // third is a string of 50
+                int id = 1;
+                string nombre = "Isaac".PadRight(30); // Pad to make the size of the string fixed
+                string apellido = "Ramirez".PadRight(50);
+
+                writer.Write(id);
+                writer.Write(nombre);   
+                writer.Write(apellido);        
+            }
+            return OperationStatus.Success;
+        }
+
+        public OperationStatus insert()
+        {
+            // Creates a default DB called TESTDB
+            Directory.CreateDirectory($@"{DataPath}\TESTDB");
+
+            // Creates a default Table called ESTUDIANTES
+            var tablePath = $@"{DataPath}\TESTDB\ESTUDIANTES.Table";
+
+            using (FileStream stream = File.Open(tablePath, FileMode.OpenOrCreate))
+            using (BinaryWriter writer = new (stream))
+            {
+                // Create an object with a hardcoded.
+                // First field is an int, second field is a string of size 30,
+                // third is a string of 50
+                int id = 1;
+                string nombre = "Isaac".PadRight(30); // Pad to make the size of the string fixed
+                string apellido = "Ramirez".PadRight(50);
+
+                writer.Write(id);
+                writer.Write(nombre);   
+                writer.Write(apellido);        
+            }
+            return OperationStatus.Success;
+        }
+
+        public OperationStatus Select()
+        {
+            // Creates a default Table called ESTUDIANTES
+            var tablePath = $@"{DataPath}\TESTDB\ESTUDIANTES.Table";
+            using (FileStream stream = File.Open(tablePath, FileMode.OpenOrCreate))
+            using (BinaryReader reader = new (stream))
+            {
+                // Print the values as a I know exactly the types, but this needs to be done right
+                Console.WriteLine(reader.ReadInt32());
+                Console.WriteLine(reader.ReadString());
+                Console.WriteLine(reader.ReadString());
+                return OperationStatus.Success;
+            }
+        }
+    }
+}
